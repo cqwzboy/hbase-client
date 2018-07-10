c0fea91173eb35da89a8d96a5ac9e7aed24fdff1
fuqinqin <904571943@qq.com>
2018-07-10 16:37:32 +0800

    2018-07-10 更新

--- src/main/java/com/qc/itaojin/common/HBaseErrorCode.java
+++ src/main/java/com/qc/itaojin/common/HBaseErrorCode.java
@@ -1,13 +1,27 @@
 package com.qc.itaojin.common;
 
 /**
- * Created by fuqinqin on 2018/7/10.
+ * @desc 错误码
+ * @author fuqinqin
+ * @date 2018-07-10
  */
 public class HBaseErrorCode {
 
+    /**
+     * 表不存在
+     * */
     public static final int TABLE_NOT_FOUND = 100;
+    /**
+     * 修改版本号失败
+     * */
     public static final int UPDATE_VERSIONS_FAILED = 101;
+    /**
+     * 修改数据失败
+     * */
     public static final int UPDATE_FAILED = 102;
+    /**
+     * 删除数据失败
+     * */
     public static final int DELETE_FAILED = 103;
 
 }
