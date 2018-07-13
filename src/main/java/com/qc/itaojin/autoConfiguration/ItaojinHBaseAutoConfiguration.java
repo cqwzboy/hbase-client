3d687ca25472a92451c1ba96107a80cae6887154
fuqinqin <904571943@qq.com>
2018-07-14 05:35:44 +0800

    2018-07-14 新增注解功能 - 1

--- src/main/java/com/qc/itaojin/autoConfiguration/ItaojinHBaseAutoConfiguration.java
+++ src/main/java/com/qc/itaojin/autoConfiguration/ItaojinHBaseAutoConfiguration.java
@@ -5,6 +5,7 @@ import com.qc.itaojin.config.ItaojinHBaseConfig;
 import com.qc.itaojin.config.ItaojinZKConfig;
 import com.qc.itaojin.service.IHBaseService;
 import com.qc.itaojin.service.impls.HBaseServiceImpl;
+import com.qc.itaojin.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,6 +13,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
 
 /**
  * Created by fuqinqin on 2018/7/3.
@@ -30,6 +33,7 @@ public class ItaojinHBaseAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public IHBaseService ihBaseService(){
+        show();
         IHBaseService hBaseService = new HBaseServiceImpl();
         ((HBaseServiceImpl) hBaseService).setPool(hBaseConnectionPool());
         return hBaseService;
@@ -44,6 +48,15 @@ public class ItaojinHBaseAutoConfiguration {
         // 初始化
         hBaseConnectionPool.init();
         return hBaseConnectionPool;
+    }
+
+    private void show(){
+        String pak = "com.qc.itaojin.canalclient.test";
+        URL url = ItaojinHBaseAutoConfiguration.class.getClassLoader().getResource("");
+        String absolutePath = url.getPath();
+        String a = pak.replaceAll("\\.", "/");
+        String path = StringUtils.contact(absolutePath, a);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>. hbase : "+path);
     }
 
 }
